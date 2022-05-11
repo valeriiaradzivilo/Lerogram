ae5d866c19b2d99dee49a84a1422eeb1422acb3b
valeriiaradzivilo <v.radzivilo.ip14@kpi.ua>
2022-05-11 16:53:49 +0300

    Added make rand with the amount of yel_dots, make retry button

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import funcs_logic
 import funcs_interface
 from classes import WindowMaker, CheckButton
@@ -16,17 +18,21 @@ funcs_interface.make_window_label_board(main_window)
 funcs_interface.make_two_color_boxes()
 # create an array for all pressed buttons and their color
 all_coords = []
-yel_dots = funcs_logic.generate_yel_dots(100)
+# generate yellow dots in amount of random range
+yel_dots = funcs_logic.generate_yel_dots(randint(100, 200))
+# solution
 print("Answer:", yel_dots)
+# depending on the placement of yellow dots count maximum amounts for boxes
 bl_horiz, yel_horiz = funcs_logic.find_max_hor(yel_dots)
 bl_vert, yel_vert = funcs_logic.find_max_vert(yel_dots)
-
+# create vertical and horizontal lines of numbers
 funcs_interface.make_num_horiz_board(bl_horiz, yel_horiz)
 funcs_interface.make_num_vert_board(bl_vert, yel_vert)
-
+# create field with buttons
 funcs_interface.create_button_field(all_coords)
-
 # work with an array all_coords
+# button to check user input
 CheckButton(all_coords, bl_horiz, yel_horiz, bl_vert, yel_vert)
-
+# if the answer is correct right_answer window will appear
+# otherwise fail_window
 main_window.mainloop()
